ebb1118e9106ec19e9240f09135fc255904bfda8
rmalik <rmalik@pinterest.com>
2017-03-24 14:38:06 -0700

    Access schema using NSFileCoordinator to synchronize schema reads against parallel invocations of plank.

--- Sources/Core/SchemaLoader.swift
+++ Sources/Core/SchemaLoader.swift
@@ -26,30 +26,39 @@ class FileSchemaLoader: SchemaLoader {
             return cachedValue
         }
         // Load from local file
-        if let data = try? Data(contentsOf: URL(fileURLWithPath: schemaUrl.path)) {
-            if let jsonResult = try? JSONSerialization.jsonObject(with: data,
-                                                                  options: JSONSerialization.ReadingOptions.mutableContainers) {
-                if let jsonDict = jsonResult as? JSONObject {
-                    let id = jsonDict["id"] as? String ?? ""
-                    if id.hasSuffix(schemaUrl.lastPathComponent) == false {
-                        fatalError("Invalid Schema: The value for the `id` (\(id) must end with the filename \(schemaUrl.lastPathComponent).")
-                    }
+        var error: NSError?
+        var loadedSchema: Schema? = nil
+        NSFileCoordinator().coordinate(readingItemAt: URL(fileURLWithPath: schemaUrl.path),
+                                       options: .forUploading,
+                                       error: &error) { (sandboxUrl: URL) in
+                                        if let data = try? Data(contentsOf: URL(fileURLWithPath: sandboxUrl.path)) {
+                                            if let jsonResult = try? JSONSerialization.jsonObject(with: data,
+                                                                                                  options: JSONSerialization.ReadingOptions.mutableContainers) {
+                                                if let jsonDict = jsonResult as? JSONObject {
+                                                    let id = jsonDict["id"] as? String ?? ""
+                                                    if id.hasSuffix(schemaUrl.lastPathComponent) == false {
+                                                        fatalError("Invalid Schema: The value for the `id` (\(id) must end with the filename \(schemaUrl.lastPathComponent).")
+                                                    }
 
-                    if let schema = FileSchemaLoader.sharedPropertyLoader(jsonDict, schemaUrl) {
-                        refs[schemaUrl] = schema
-                        return schema
-                    } else {
-                        fatalError("Invalid Schema. Unable to parse schema at URL: \(schemaUrl)")
-                    }
-                } else {
-                    fatalError("Invalid Schema. Expected dictionary as the root object type for schema at URL: \(schemaUrl)")
-                }
-            } else {
-               fatalError("Invalid JSON. Unable to parse json at URL: \(schemaUrl)")
-            }
-        } else {
-            fatalError("Error loading or parsing schema at URL: \(schemaUrl)")
+                                                    if let schema = FileSchemaLoader.sharedPropertyLoader(jsonDict, schemaUrl) {
+                                                        self.refs[schemaUrl] = schema
+                                                        loadedSchema = schema
+                                                    } else {
+                                                        fatalError("Invalid Schema. Unable to parse schema at URL: \(schemaUrl)")
+                                                    }
+                                                } else {
+                                                    fatalError("Invalid Schema. Expected dictionary as the root object type for schema at URL: \(schemaUrl)")
+                                                }
+                                            } else {
+                                                fatalError("Invalid JSON. Unable to parse json at URL: \(schemaUrl)")
+                                            }
+                                        } else {
+                                            fatalError("Error loading or parsing schema at URL: \(schemaUrl)")
+                                    }
         }
-        return nil
+        if (error != nil) {
+            fatalError("Error accessing schema at URL: \(schemaUrl)")
+        }
+        return loadedSchema
     }
 }
